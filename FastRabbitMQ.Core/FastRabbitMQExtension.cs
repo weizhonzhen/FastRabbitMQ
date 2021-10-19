da0178ff245bf4ea9c70d78f00856a4637cc6087
weizhonzhen <czdswzz@126.com>
2021-10-19 20:47:03 +0800

    Add files via upload

---
+++ FastRabbitMQ.Core/FastRabbitMQExtension.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using FastRabbitMQ.Core;
+using FastRabbitMQ.Core.Aop;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FastRabbitMQExtension
+    {
+        private static ConfigData config;
+        public static IServiceCollection AddFastRabbitMQ(this IServiceCollection serviceCollection, Action<ConfigData> action)
+        {
+            config = new ConfigData();
+            action(config);
+
+            IConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = config.Host,
+                Port = config.Port,
+                UserName = config.UserName,
+                Password = config.PassWord,
+                VirtualHost = config.VirtualHost,
+                AutomaticRecoveryEnabled = true
+            };
+
+            var conn = factory.CreateConnection();
+
+            if (config.aop == null)
+                throw new Exception("AddFastRabbitMQ aop not null");
+
+            serviceCollection.AddSingleton<IConnection>(conn);
+            serviceCollection.AddSingleton<IFastRabbitAop>(config.aop);
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+            return serviceCollection;
+        }
+    }
+
+    public class ConfigData
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; } = 5672;
+
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public string VirtualHost { get; set; } = "/";
+
+        public IFastRabbitAop aop { get; set; }
+    }
+}
---
+++ FastRabbitMQ.Core/ServiceEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FastRabbitMQ.Core
+{
+    internal class ServiceEngine : IServiceEngine
+    {
+        private IServiceProvider serviceProvider;
+        public ServiceEngine(IServiceProvider _serviceProvider)
+        {
+            this.serviceProvider = _serviceProvider;
+        }
+
+        public T Resolve<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+    }
+
+    internal interface IServiceEngine
+    {
+        T Resolve<T>();
+    }
+
+    internal class ServiceContext
+    {
+        private static IServiceEngine engine;
+        public static IServiceEngine Init(IServiceEngine _engine)
+        {
+            engine = _engine;
+            return engine;
+        }
+
+        public static IServiceEngine Engine
+        {
+            get
+            {
+                return engine;
+            }
+        }
+    }
+}
